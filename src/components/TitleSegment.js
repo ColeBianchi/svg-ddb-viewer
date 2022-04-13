10ca4f02930699c84fb8f338fc3f851a8e6eda9e
Cole Bianchi <cmb7256@psu.edu>
2022-04-12 20:03:51 -0400

    Working GUN implementation

--- src/components/TitleSegment.js
+++ src/components/TitleSegment.js
@@ -5,12 +5,23 @@ const { Title } = Typography;
 const { Option } = Select;
 
 //Title segment container
-export default function TitleSegment({})
+export default function TitleSegment({ svgTitle, svgNameList, changeCurrentSVG })
 {
 	//Return title segment
 	return(
 		<>
 			<Title>Decentralized Database for SVG images</Title>
+
+			<Title level={2}>Current display: <b>{svgTitle}</b></Title>
+
+			<Select defaultValue={svgNameList[0]}
+					style={{ width: 360 }}
+					onChange={changeCurrentSVG} >
+				{svgNameList.map((svgName) => 
+				{
+					return <Option value={svgName}>{svgName}</Option>;
+				})}
+			</Select>
 		</>
 	);
 }
--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -7,7 +7,9 @@ export default function Viewer({ svgData })
 	//Return JSX for svg display
 	return(
 		<>
-			<h1>This is the viewer</h1>
+			<img 	src={`data:image/svg+xml;utf8,${encodeURIComponent(svgData)}`}
+					alt="Error loading SVG"
+					style={{ maxWidth: 420 }} />
 		</>
 	)
 }
